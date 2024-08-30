0f46927165fab3bb168977cba35fa115ea543129
rgelpi4ever <137299600+rgelpi4ever@users.noreply.github.com>
2024-08-30 17:28:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #ST.STOP()
-pd_df = my_dataframe.to_pandas()
+pd_df = my_dataframe.topandas()
 #pd.dataframe(pd_df)
 #st.stop()
 
