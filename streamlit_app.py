69382f8b5174209789ac9693fdfb44795a9dcf3e
rgelpi4ever <137299600+rgelpi4ever@users.noreply.github.com>
2024-09-02 11:58:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,9 @@ if ingredients_list:
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
             fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
-    st.write(ingredients_string)
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-    values ('""" + ingredients_string + """')"""
+        st.write(ingredients_string)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+        values ('""" + ingredients_string + """')"""
     st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
     #st.stop()
