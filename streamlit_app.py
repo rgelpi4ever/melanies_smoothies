e67a9ccab757597bed08059d32362fc863fdc496
rgelpi4ever <137299600+rgelpi4ever@users.noreply.github.com>
2024-09-02 12:10:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,24 +30,33 @@ ingredients_list = st.multiselect(
     )
 
 if ingredients_list:
-        ingredients_string = ''
-        for fruit_chosen in ingredients_list:
-            ingredients_string += fruit_chosen + ' '
+#    st.write(ingredients_list)
+#    st.text(ingredients_list)
+
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+#        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        
-            st.subheader(fruit_chosen + 'Nutrition Information')
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-        
-        st.write(ingredients_string)
-        #my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-        #values ('""" + ingredients_string + """')"""
-        #st.write(my_insert_stmt)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+#    st.write(ingredients_string)
+
+#    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+#            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
+
+#    st.write(my_insert_stmt)
+#    st.stop()
+    
     time_to_insert = st.button('Submit Order')
-        #st.stop()
-        #    if ingredients_string:
+#    if ingredients_string:
     message_string = ''
     message_string += 'Your Smoothie is ordered,'
     message_string += ' '
@@ -56,5 +65,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(message_string, icon ="✅")
-
-
