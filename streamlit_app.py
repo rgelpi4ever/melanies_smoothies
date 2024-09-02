f8d243ec6fa1953c96b647e3d01117a4d0a9e9b8
rgelpi4ever <137299600+rgelpi4ever@users.noreply.github.com>
2024-09-02 09:30:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,10 @@ st.write(
 from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
-st.dataframe(data=my_dataframe, use_container_width=True)
-ST.STOP()
-#pd_df = my_dataframe.to_pandas()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+##ST.STOP()
+pd_df = my_dataframe.to_pandas()
 #pd.dataframe(pd_df)
 #st.stop()
 
