f2d9e64fa310e8bf8cce93a295c5d6f2f239cb3f
rgelpi4ever <137299600+rgelpi4ever@users.noreply.github.com>
2024-09-02 10:43:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,19 +27,10 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe
                                   ,max__sections=5
                                  )
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-st.write(ingredients_string)
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-values ('""" + ingredients_string + """')"""
-st.write(my_insert_stmt)
-time_to_insert = st.button('Submit Order')
-#st.stop()
-
 
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
+    st.write(ingredients_list)
+    st.text(ingredients_list)
     ingredients_string =''
     
     for fruit_chosen in ingredients_list:
@@ -47,6 +38,7 @@ if ingredients_list:
        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
        st.subheader(fruit_chosen + 'Nutrition Information')
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 st.write(ingredients_string)
