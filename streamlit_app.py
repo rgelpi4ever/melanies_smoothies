1a0d9e53d7d74142f30e457ad193ad69b5a2a95b
rgelpi4ever <137299600+rgelpi4ever@users.noreply.github.com>
2024-09-02 09:59:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,9 @@ pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
-ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe)
+ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe
+                                  ,max__sections=5
+                                 )
 
 if ingredients_list:
     #st.write(ingredients_list)
